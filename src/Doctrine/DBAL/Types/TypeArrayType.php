7f5d73e7a2c30fa32e65bc4548c5e5a5143aef9e
Randhir Gulraj <randh1r@hotmail.com>
2025-02-27 12:19:29 +0100

    fix: improve type annotations and thrown exceptions

--- src/Doctrine/DBAL/Types/TypeArrayType.php
+++ src/Doctrine/DBAL/Types/TypeArrayType.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Jaytaph\TypeArray\Doctrine\DBAL\Types;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Exception\InvalidType;
+use Doctrine\DBAL\Types\Exception\SerializationFailed;
+use Doctrine\DBAL\Types\Exception\ValueNotConvertible;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\Deprecations\Deprecation;
 use JsonException;
@@ -25,7 +27,7 @@ class TypeArrayType extends Type
     /**
      * {@inheritdoc}
      */
-    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
     {
         return $platform->getJsonTypeDeclarationSQL($column);
     }
@@ -33,27 +35,27 @@ class TypeArrayType extends Type
     /**
      * {@inheritdoc}
      */
-    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): false|string|null
     {
         if ($value === null) {
             return null;
         }
 
         if (! $value instanceof TypeArray) {
-            throw ConversionException::conversionFailedInvalidType($value, self::TYPE, ['null', TypeArray::class]);
+            throw InvalidType::new($value, self::TYPE, ['null', TypeArray::class]);
         }
 
         try {
             return json_encode($value->toArray(), JSON_THROW_ON_ERROR | JSON_PRESERVE_ZERO_FRACTION);
         } catch (JsonException $e) {
-            throw ConversionException::conversionFailedSerialization($value, self::TYPE, $e->getMessage(), $e);
+            throw SerializationFailed::new($value, self::TYPE, $e->getMessage(), $e);
         }
     }
 
     /**
      * {@inheritdoc}
      */
-    public function convertToPHPValue($value, AbstractPlatform $platform)
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?TypeArray
     {
         if ($value === null || $value === '') {
             return null;
@@ -66,14 +68,14 @@ class TypeArrayType extends Type
         try {
             return TypeArray::fromJson(strval($value));
         } catch (JsonException $e) {
-            throw ConversionException::conversionFailed($value, $this->getName(), $e);
+            throw ValueNotConvertible::new($value, $this->getName(), $e->getMessage(), $e);
         }
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getName()
+    public function getName(): string
     {
         return self::TYPE;
     }
@@ -83,7 +85,7 @@ class TypeArrayType extends Type
      *
      * @deprecated
      */
-    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
     {
         Deprecation::triggerIfCalledFromOutside(
             'doctrine/dbal',
